d0b1365dca57043c5b9d0b85e5c772641415e3a1
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:18:41 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,1 @@
-# variables.tf
-
-variable "aws_region" {
-  description = "The AWS region to create resources in."
-  default     = "us-west-2"
-}
-
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instance."
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The instance type for the EC2 instance."
-  type        = string
-  default     = "t2.micro"
-}
+#No variables defined for this simple example
