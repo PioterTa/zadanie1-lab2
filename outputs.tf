ed08292ce6a4417ce2018a292ac90de8f154c39a
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:18:15 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-# outputs.tf
-
-output "instance_id" {
-  description = "The ID of the EC2 instance."
-  value       = aws_instance.example_instance.id
-}
-
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance."
-  value       = aws_instance.example_instance.public_ip
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
 }
