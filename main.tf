18243df251eef97e933b4876aaf047a88fcf1332
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:26:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,12 @@ resource "aws_s3_bucket_acl" "my_bucket_acl" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "example" {
-  bucket = "my-localstack-bucket"
+resource "aws_s3_object" "example" {
+  bucket = aws_s3_bucket_acl.my_bucket_acl.bucket
   key    = "example.txt"
   content = "This is an example file."
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket_acl.my_bucket_acl.bucket
 }
