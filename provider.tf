f3649de05619b10bce9e3951fe222a9e3bf28996
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:18:29 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,20 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Use the appropriate version
+    }
+  }
 }
 
-variable "aws_region" {
-  description = "The AWS region to create resources in."
-  default     = "us-west-2"
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566/"
+  }
 }
