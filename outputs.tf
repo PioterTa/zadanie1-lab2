666e3c8cf4d03f05e6a5ae28b2249c16cafc9bef
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:11:08 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
-output "example_output" {
-  description = "An example output from your Terraform state."
+# outputs.tf
+
+output "instance_id" {
+  description = "The ID of the EC2 instance."
   value       = aws_instance.example_instance.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance."
+  value       = aws_instance.example_instance.public_ip
 }
