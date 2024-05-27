fa99a7dfb5c300a0ad68ccdb8ce8419ebd75a015
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-27 16:18:32 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"  # Use the appropriate version
-    }
-  }
-}
-
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-
+  s3_force_path_style         = true
   endpoints {
-    s3 = "http://localhost:4566/"
+    s3 = "http://localhost:4566"
   }
 }
